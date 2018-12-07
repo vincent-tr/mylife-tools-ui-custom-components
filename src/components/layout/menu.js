9e06f5e05f2b9f1715ab224af5b1bd3063598d2c
vincent-tr <vincent@stoic.com>
2018-12-07 16:45:19 +0100

    menu wip

--- src/components/layout/menu.js
+++ src/components/layout/menu.js
@@ -2,15 +2,29 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const Menu = ({ items }) => (
-  <div className='menu'>
-    {JSON.stringify(items)}
-  </div>
+  <ul className='menu'>
+    {items.map(({ id, text, icon, onClick, type }) => (
+      <li key={id} onClick={() => onClick(id)} className={classNames(type, { clickable: onClick })}>
+        {icon && <div className='icon'>{icon}</div>}
+        <div className='text'>{text}</div>
+      </li>
+    ))}
+  </ul>
 );
 
 Menu.propTypes = {
-  items: PropTypes.array // TODO: better validation
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id      : PropTypes.string.isRequired,
+      text    : PropTypes.node.isRequired,
+      icon    : PropTypes.node,
+      onClick : PropTypes.func,
+      type    : PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
 };
 
 export default Menu;
