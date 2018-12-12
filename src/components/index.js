68398b4731e63c66a64eda25c1a983b28d61ff67
vincent-tr <vincent@stoic.com>
2018-12-12 11:44:34 +0100

    move stuff

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import 'normalize.css';
+import 'ubuntu-fontface/_ubuntu-base.scss';
+import './main.scss';
+
+export * from './icon';
+export * from './button';
+export * from './layout';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 'use strict';
 
-import 'normalize.css';
-import 'ubuntu-fontface/_ubuntu-base.scss';
-import '../src/main.scss';
-
-export * from './components/icon';
-export * from './components/button';
-export * from './components/layout';
+export * from './components';
