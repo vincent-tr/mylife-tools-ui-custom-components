4b5cd5ab13e3b665ffe2da2bf4a55412053f83f5
vincent-tr <vincent@stoic.com>
2019-01-14 16:16:21 +0100

    cleanup

--- src/modules/dialogs/helpers.js
+++ src/modules/dialogs/helpers.js
@@ -7,29 +7,13 @@ export async function showDialog(Impl) {
   return new Promise(accept => {
     const node = document.createElement('div');
 
-    class Root extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          open: true
-        };
-      }
-
-      onClose = (result) => {
-        this.setState({ open: false });
-        ReactDOM.unmountComponentAtNode(node);
-        document.body.removeChild(node);
-        accept(result);
-      }
-
-      render() {
-        return (
-          <Impl open={this.state.open} onClose={this.onClose} />
-        );
-      }
-    }
+    const onClose = (result) => {
+      ReactDOM.unmountComponentAtNode(node);
+      document.body.removeChild(node);
+      accept(result);
+    };
 
     document.body.appendChild(node);
-    ReactDOM.render(<Root />, node);
+    ReactDOM.render(<Impl open={true} onClose={onClose} />, node);
   });
 }
