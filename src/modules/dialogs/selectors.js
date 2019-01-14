d91abf2fe711dc93a5262f4f6dcf46ebd5bf4548
vincent-tr <vincent@stoic.com>
2019-01-14 11:12:07 +0100

    add selector

--- src/modules/dialogs/selectors.js
+++ src/modules/dialogs/selectors.js
@@ -4,6 +4,7 @@ import { getInternalState } from '../../selectors/base';
 
 const getDialogs = state => getInternalState(state);
 
+export const getBusy          = state => getDialogs(state).busy;
 export const getError         = state => getDialogs(state).error;
 export const getInfo          = state => getDialogs(state).info;
 export const getNotifications = state => getDialogs(state).notifications;
