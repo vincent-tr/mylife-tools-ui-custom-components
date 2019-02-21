8671959468e2b787758ee854963797841ca8ed8b
vincent-tr <vincent@stoic.com>
2019-02-21 18:51:57 +0100

    stretch by default

--- demo/components/views/containers.js
+++ demo/components/views/containers.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { components } from 'mylife-tools-ui';
 
 const Containers = () => (
-  <components.GroupBox stretch title='Titre'>
-    <components.Splitter stretch>
+  <components.GroupBox title='Titre'>
+    <components.Splitter>
       <div>Pane1</div>
       <div>Pane2</div>
     </components.Splitter>
