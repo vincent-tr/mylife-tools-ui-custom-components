e410d073882493e993ebb0050214675c5504d1ef
vincent-tr <vincent@stoic.com>
2019-03-15 13:49:05 +0100

    wip slider

--- src/components/editors/integer-slider.js
+++ src/components/editors/integer-slider.js
@@ -12,6 +12,11 @@ const valueToEditor = createValueToEditor(x => x.toString());
 const editorToValue = createEditorToValue(parseInt, 0);
 
 const IntegerSlider = React.forwardRef(({ containerClassName, className, enabled, readOnly, nullable, value, onChange, min, max, ...props }, ref) => {
+  const range = max - min + 1;
+  if(range > 20) {
+    throw new Error('slider range should max 20');
+  }
+
   const [focus, setFocus] = useState(false);
   const [hover, setHover] = useState(false);
   const showButtons = enabled && !readOnly && (hover || focus);
@@ -37,7 +42,7 @@ const IntegerSlider = React.forwardRef(({ containerClassName, className, enabled
         max={max}
         value={valueToEditor(nullable, value)}
         onChange={e => onChange(editorToValue(nullable, e.target.value))}
-        className={classNames('editor-component', 'integer-slider', { 'value-null': value === null }, className)}
+        className={classNames('editor-component', 'integer-slider', `range-${range}`, { 'value-null': value === null }, className)}
         disabled={!enabled}
         readOnly={readOnly}
         onFocus={handleFocus}
