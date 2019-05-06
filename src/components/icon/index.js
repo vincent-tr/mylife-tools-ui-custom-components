9678b551122972efe8b58be4ac07c9b7821b44df
vincent-tr <vincent.tr@gmail.com>
2019-05-06 16:44:51 +0200

    add react-icons

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -5,3 +5,13 @@ import './main.scss';
 export Icon from './icon';
 export SvgIcon from './svg-icon';
 export * from './resources';
+
+export * from 'react-icons/fa';
+export * from 'react-icons/io';
+export * from 'react-icons/md';
+export * from 'react-icons/ti';
+export * from 'react-icons/go';
+export * from 'react-icons/fi';
+export * from 'react-icons/gi';
+export * from 'react-icons/wi';
+export * from 'react-icons/di';
