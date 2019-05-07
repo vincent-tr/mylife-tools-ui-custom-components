92e0c051fd22326b69e605fdab2eee0f66e93292
vincent-tr <vincent.tr@gmail.com>
2019-05-07 10:41:20 +0200

    table wip

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,23 +1,40 @@
 'use strict';
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FixedSizeList } from 'react-window';
-import { AutoSizer } from '../utils';
+import { AutoSizer, Measure } from '../utils';
 
 import './table.scss';
 
 const Table = React.forwardRef(({ className, columns, data, ...otherProps }, ref) => {
+  const [itemSize, setItemSize] = useState(1);
+
+  const onRowMeasure=contentRect => {
+    const { height } = contentRect.bounds;
+    if(itemSize < height) {
+      setItemSize(height);
+    }
+  };
+
+  const Row = ({ index, style }) => (
+    <div style={style}>
+      <Measure bounds onResize={onRowMeasure}>
+        {({ measureRef }) => (
+          <div ref={measureRef}>
+            {JSON.stringify(data[index])}
+          </div>
+        )}
+      </Measure>
+    </div>
+  );
+
   return (
     <AutoSizer>
       {({ height, width }) => (
-        <FixedSizeList height={height} width={width} itemCount={data.length} itemSize={35}>
-          {({ index, style }) => (
-            <div style={style}>
-              {index}
-            </div>
-          )}
+        <FixedSizeList height={height} width={width} itemCount={data.length} itemSize={itemSize}>
+          {Row}
         </FixedSizeList>
       )}
     </AutoSizer>
